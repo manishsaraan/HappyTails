3a235d8d7aa99cfec957ad47ea4f4bbfa3ffc15c
manishsaraan <manish021js@gmail.com>
2024-07-07 18:32:14 +0530

    add shadcn ui

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -21,7 +23,14 @@ export default function Home() {
           Use Petsoft to easily keep track of pets under your care. Get lifetime
           of access for $299
         </p>
-        <div className="mt-10"></div>
+        <div className="mt-10 space-x-3">
+          <Button asChild>
+            <Link href={"/signup"}>Get Started</Link>
+          </Button>
+          <Button asChild variant={"secondary"}>
+            <Link href={"/login"}>Log In</Link>
+          </Button>
+        </div>
       </div>
     </main>
   );
