c6a4d66aa3bb4f0e4bb2df2124d58d29bac2e3eb
manishsaraan <manish021js@gmail.com>
2024-07-17 14:56:18 +0530

    fetch pets from the db

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -3,27 +3,21 @@ import AppHeader from "@/components/app-header";
 import BackgroundPattern from "@/components/background-pattern";
 import PetContextProvider from "@/contexts/pet-context-provider";
 import SearchContextProvider from "@/contexts/search-context-provider";
+import Prisma from "@/lib/db";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
-  );
+  const pets = await Prisma.pet.findMany();
 
-  if (!response.ok) {
-    throw new Error("Could not fetch pets");
-  }
-
-  const data = await response.json();
   return (
     <>
       <BackgroundPattern />
       <div className="max-w-[1050px] mx-auto flex flex-col min-h-screen">
         <AppHeader />
-        <PetContextProvider pets={data}>
+        <PetContextProvider pets={pets}>
           <SearchContextProvider>{children}</SearchContextProvider>
         </PetContextProvider>
 
