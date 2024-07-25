5faba81484e37a315967d584bbc21600632249e3
manishsaraan <manish021js@gmail.com>
2024-07-25 17:28:24 +0530

    add react-hook-form with validations

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import {
   DialogHeader,
   DialogTitle,
@@ -11,7 +12,6 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Pet, PetData } from "@/lib/types";
-import { useFormStatus } from "react-dom";
 import { usePetContext } from "@/hooks/pet-context-hook";
 
 /**
@@ -28,80 +28,136 @@ export default function PetForm({
   onFormSubmission: () => void;
   selectedPet?: Pet;
 }) {
-  // const [state, formAction] = useFormState(addPetAction, {});
   const { handleAddPet, handleEditPet } = usePetContext();
-  const handleSubmit = async (formData: FormData) => {
-    console.log(formData);
-    onFormSubmission(); // closing it before action because of optimitic ui
-    const petData = {
-      name: formData.get("name") as string,
-      age: parseInt(formData.get("age") as string),
-      ownerName: formData.get("ownerName") as string,
-      imageUrl: formData.get("imageUrl") as string,
-      notes: formData.get("notes") as string,
-    } as PetData;
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = useForm<PetData>({
+    defaultValues: {
+      name: selectedPet?.name || "",
+      age: selectedPet?.age || 0,
+      ownerName: selectedPet?.ownerName || "",
+      imageUrl: selectedPet?.imageUrl || "",
+      notes: selectedPet?.notes || "",
+    },
+  });
 
+  const onSubmit: SubmitHandler<PetData> = async (data) => {
+    onFormSubmission(); // closing it before action because of optimistic UI
     let result;
     if (actionType === "add") {
-      result = await handleAddPet(petData);
+      result = await handleAddPet(data);
     } else if (actionType === "edit") {
-      result = await handleEditPet(petData, selectedPet!.id);
+      result = await handleEditPet(data, selectedPet!.id);
     }
   };
+
   return (
     <>
       <DialogHeader>
         <DialogTitle>{heading}</DialogTitle>
       </DialogHeader>
-      <form action={handleSubmit}>
-        <div className="space-y-1  mb-4">
+      <form
+        // onSubmit={handleSubmit(onSubmit)}
+        action={async (formData: FormData) => {
+          const result = await trigger();
+          if (!result) {
+            return;
+          }
+          const formDataObj: PetData = {
+            name: formData.get("name") as string,
+            ownerName: formData.get("ownerName") as string,
+            imageUrl: formData.get("imageUrl") as string,
+            age: parseInt(formData.get("age") as string),
+            notes: formData.get("notes") as string,
+          };
+
+          await onSubmit(formDataObj);
+        }}
+      >
+        <div className="space-y-1 mb-4">
           <Label htmlFor="name">Name</Label>
           <Input
             id="name"
-            name="name"
-            type="text"
-            defaultValue={actionType === "edit" ? selectedPet?.name : ""}
-            required
+            {...register("name", {
+              required: "Name is required",
+              minLength: {
+                value: 2,
+                message: "Name must be at least 2 characters long",
+              },
+              maxLength: {
+                value: 20,
+                message: "Name must be at most 20 characters long",
+              },
+            })}
           />
+          {errors.name && (
+            <span className="text-red-500">{errors.name.message}</span>
+          )}
         </div>
-        <div className="space-y-1  mb-4">
+        <div className="space-y-1 mb-4">
           <Label htmlFor="owner">Owner</Label>
           <Input
             id="owner"
-            name="ownerName"
-            type="text"
-            defaultValue={actionType === "edit" ? selectedPet?.ownerName : ""}
-            required
+            {...register("ownerName", {
+              required: "Owner is required",
+              minLength: {
+                value: 2,
+                message: "Owner must be at least 2 characters long",
+              },
+            })}
           />
+          {errors.ownerName && (
+            <span className="text-red-500">{errors.ownerName.message}</span>
+          )}
         </div>
-        <div className="space-y-1  mb-4">
+        <div className="space-y-1 mb-4">
           <Label htmlFor="image-url">Image URL</Label>
           <Input
             id="image-url"
-            name="imageUrl"
-            type="url"
-            defaultValue={actionType === "edit" ? selectedPet?.imageUrl : ""}
-            required
+            {...register("imageUrl", {
+              required: "Image URL is required",
+            })}
           />
+          {errors.imageUrl && (
+            <span className="text-red-500">{errors.imageUrl.message}</span>
+          )}
         </div>
-        <div className="space-y-1  mb-4">
+        <div className="space-y-1 mb-4">
           <Label htmlFor="age">Age</Label>
           <Input
             id="age"
-            name="age"
             type="number"
-            defaultValue={actionType === "edit" ? selectedPet?.age : ""}
-            required
+            {...register("age", {
+              required: "Age is required",
+              valueAsNumber: true,
+            })}
           />
+          {errors.age && (
+            <span className="text-red-500">{errors.age.message}</span>
+          )}
         </div>
-        <div className="space-y-1  mb-4">
+        <div className="space-y-1 mb-4">
           <Label htmlFor="notes">Notes</Label>
           <Textarea
             id="notes"
-            name="notes"
-            defaultValue={actionType === "edit" ? selectedPet?.notes : ""}
-            required
+            {...register("notes", {
+              required: "Notes are required",
+              minLength: {
+                value: 2,
+                message: "Notes must be at least 2 characters long",
+              },
+              maxLength: {
+                value: 100,
+                message: "Notes must be at most 100 characters long",
+              },
+            })}
           />
+          {errors.notes && (
+            <span className="text-red-500">{errors.notes.message}</span>
+          )}
         </div>
         <DialogFooter>
           <SubmitButton actionType={actionType} />
